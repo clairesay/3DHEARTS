bed8e8858c93a3d17e4a74459b4d1eeb596ad79e
uknable <uknable@hotmail.com>
2020-05-06 16:53:21 +1000

    added express

--- index.js
+++ index.js
@@ -1,68 +1,26 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const url = require('url');
-
-const server = http.createServer((req, res) => {
-
-    // Build file path
-    let filePath = path.join(
-        __dirname, 
-        'public', 
-        req.url === '/' ? 'index.html' : req.url
-    );
-
-    // Extension of file
-    let extname = path.extname(filePath);
-
-    //Initial content type
-    let contentType = 'text/html';
-
-    //Check ext and set content type
-    switch(extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-    }
-
-    //Read File
-    fs.readFile(filePath, (err, content) => {
-        if(err) {
-            if(err.code == 'ENOENT') {
-                //Page not found
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html'});
-                    res.end(content, 'utf8');
-                })
-            } else {
-                // Some server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-            }
-        } else {
-            // success
-            res.writeHead(200, { 'Content-Type': contentType });
-            console.log(path.parse(__filename).name);
-            res.end(content, 'utf8');
-
-        }
-    })
-});
-
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// app.engine('.hbs', exphbs({
+//     defaultLayout: 'main',
+//     extname: '.hbs',
+//     partialsDir: 'views/components'
+// }));
+
+// app.set('view engine', '.hbs')
+// app.set('views', 'views')
+
+app.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`)
+})
+
 
 
